14d0f5690124e9de35b3f6b753b032ed1ae91e57
Muhammed <Mhunterly@gmail.com>
2026-08-02 20:43:31 +0100

    feat: add range proof to Nargo.toml workspace members and build script

--- circuits/scripts/build.sh
+++ circuits/scripts/build.sh
@@ -106,5 +106,5 @@ build() {
 if [ "$#" -gt 0 ]; then
   for n in "$@"; do build "$n"; done
 else
-  for n in commit commit3 kyc_proof age_proof income_proof jurisdiction_proof funds_proof accreditation_proof employment_proof aggregate_proof; do build "$n"; done
+  for n in commit commit3 kyc_proof age_proof income_proof jurisdiction_proof funds_proof accreditation_proof range_proof employment_proof aggregate_proof; do build "$n"; done
 fi
